20dc8fa40ae0a86c0c3c7a74dc008532069236c6
daniel3400 <dmarcillo38@gmail.com>
2024-05-23 22:51:45 -0500

    se agrego informacion al index

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Sistema IOT';
-export const SITE_DESCRIPTION = 'sistema IOT para el monitoreo de variables ambientales ';
+export const SITE_TITLE = 'Fusenpro';
+export const SITE_DESCRIPTION = 'sitio web en de contacto para fusenpro ';
